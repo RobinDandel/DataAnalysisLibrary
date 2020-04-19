b380963a668756bcc7b4f944f1dcea61ce6b1103
RobinDandel <robindandel38@gmail.fr>
2020-04-19 14:10:09 +0200

    +plugin Jacoco (couverture de code) +docker

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int somme = 0;
@@ -5,5 +8,10 @@ public class Main {
             somme = somme + i;
         }
         System.out.println(somme);
+
+        List<String>name=new ArrayList<String>();
+        /*name.add("toto");
+        name.add("Jean");
+        name.forEach(n->System.out.println(n));*/
     }
 }
